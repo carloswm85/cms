1792da7391f9b4a8613bd9eeabb9526c11e8488f
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-05-08 20:11:48 -0300

    added components

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,24 @@
+export class Contact {
+  public id: number; // id of the contact.
+  public name: string; // name of the contact.
+  public email: string; // email address of the contact.
+  public phone: string; // phone number of the contact.
+  public imageUrl: string; // URL of the photo image of the contact.
+  public group: Contact[]; // attribute is only applicable to group contacts. It is an array of other contacts that belong to the group.
+
+  constructor(
+    id: number,
+    name: string,
+    email: string,
+    phone: string,
+    imageUrl: string,
+    group: Contact[]
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.imageUrl = imageUrl;
+    this.group = group;
+  }
+}
