756943931d087f5afe2cf2c2d89d123606daa280
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-05-15 21:47:32 -0300

    add & fix # document components, header fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './shared/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactItemComponent } from './contact-item/contact-item.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { ContactItemComponent } from './contact-item/contact-item.component';
     ContactsComponent,
     ContactDetailComponent,
     ContactListComponent,
-    ContactItemComponent
+    ContactItemComponent,
+    DocumentsComponent,
+    DocumentListComponent,
+    DocumentItemComponent,
+    DocumentDetailComponent
   ],
   imports: [
     BrowserModule
