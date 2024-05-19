984be736f70155ec65c618415745f91aa6e4f783
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-05-18 22:54:13 -0300

    update

--- src/app/shared/header.component.ts
+++ src/app/shared/header.component.ts
@@ -1,16 +1,12 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   collapsed = true;
   @Output() selectedFeatureEvent = new EventEmitter<string>();
-
-  ngOnInit(): void {
-    return;
-  }
 
   // This method is responsible for emitting or firing the selectedFeatureEvent.
   onSelected(selectedEvent: string) {
