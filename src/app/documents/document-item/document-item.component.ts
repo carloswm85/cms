3591745a3015f6151979782d383049c7f56fd2bf
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-05-18 22:44:35 -0300

    working # added full document components

--- src/app/documents/document-item/document-item.component.ts
+++ src/app/documents/document-item/document-item.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Document } from "../../models/document.model";
 
 @Component({
   selector: 'app-document-item',
   templateUrl: './document-item.component.html',
-  styleUrl: './document-item.component.css'
+  styleUrl: './document-item.component.css',
 })
 export class DocumentItemComponent {
+  @Input() document: Document;
+  @Output() selectedDocument = new EventEmitter<void>();
 
+  onSelect() {
+    this.selectedDocument.emit();
+  }
+
+  onKeyup() {
+    return;
+  }
 }
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../../models/document.model';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
+  styleUrl: './document-list.component.css',
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document('id0', 'name0', 'description0', 'url0', null),
+    new Document('id1', 'name1', 'description1', 'url1', null),
+    new Document('id2', 'name2', 'description2', 'url2', null),
+    new Document('id3', 'name3', 'description3', 'url3', null),
+  ];
 
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
