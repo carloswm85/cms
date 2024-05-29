e74d0a21bc96a530874b94a7ff0802f61d016a10
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-05-29 11:40:23 -0300

    update # contact service, return contact mock data

---
+++ src/app/contacts/contact-services/contact.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+import { MOCKCONTACTS } from '../contact-data/MOCKCONTACTS';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactService {
+  contacts: Contact[] = [];
+
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+  }
+
+  getContacts(): Contact[] {
+    return this.contacts.slice();
+  }
+
+  getContact(id: string): Contact {
+    const singleContact = this.contacts[id];
+    if (singleContact == null) return null;
+    return this.contacts[id];
+    // OR
+    /**
+     * getContact(id: string): Contact {
+     * FOR each contact in the contacts list
+     * IF contact.id equals the id THEN
+     * RETURN contact
+     * ENDIF
+     * ENDFOR
+     * RETURN null
+     * }
+     */
+  }
+}
