33e99d47bc6c14921140688f683d40a6e6b4180b
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-05-29 13:15:43 -0300

    working # contact list from service

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,33 +1,42 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Contact } from '../../models/contact.model';
+import { ContactService } from '../contact-services/contact.service';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css',
+  providers: [ContactService]
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
   @Output() selectedContactEvent = new EventEmitter<Contact>();
-  contacts: Contact[] = [
-    {
-      id: '1',
-      name: 'R. Kent Jackson',
-      email: 'jacksonk@byui.edu',
-      phone: '208-496-3771',
-      description: 'This is R. Kent Jackson description',
-      imageUrl: '../../assets/images/jacksonk.jpg',
-      group: null,
-    },
-    {
-      id: '2',
-      name: 'Rex Barzee',
-      email: 'barzeer@byui.edu',
-      phone: '208-496-3768',
-      description: 'This is Rex Barzee description',
-      imageUrl: '../../assets/images/barzeer.jpg',
-      group: null,
-    },
-  ];
+  // contacts: Contact[] = [
+  //   {
+  //     id: '1',
+  //     name: 'R. Kent Jackson',
+  //     email: 'jacksonk@byui.edu',
+  //     phone: '208-496-3771',
+  //     description: 'This is R. Kent Jackson description',
+  //     imageUrl: '../../assets/images/jacksonk.jpg',
+  //     group: null,
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'Rex Barzee',
+  //     email: 'barzeer@byui.edu',
+  //     phone: '208-496-3768',
+  //     description: 'This is Rex Barzee description',
+  //     imageUrl: '../../assets/images/barzeer.jpg',
+  //     group: null,
+  //   },
+  // ];
+  contacts: Contact[] = [];
+
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit(): void {
+    this.contacts = this.contactService.getContacts();
+  }
 
   onContactSelected(contact: Contact) {
     this.selectedContactEvent.emit(contact);
