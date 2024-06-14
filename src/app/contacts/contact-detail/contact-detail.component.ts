b35f6d50771b352c5f10f308770ed757592061d7
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-06-14 19:44:06 -0300

    update # contact-edit & contact routes

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Contact } from '../../models/contact.model';
+import { ContactService } from '../contact-services/contact.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
@@ -7,14 +9,27 @@ import { Contact } from '../../models/contact.model';
   styleUrl: './contact-detail.component.css',
 })
 export class ContactDetailComponent {
-  @Input() contact: Contact;
-  // contact: Contact = {
-  //   id: '1',
-  //   name: 'R. Kent Jackson',
-  //   email: 'jacksonk@byui.edu',
-  //   phone: '208-496-3771',
-  //   description: 'This is a description.',
-  //   imageUrl: '../../assets/images/jacksonk.jpg',
-  //   group: null,
-  // };
+  /* @Input() contact: Contact; */
+  contact: Contact;
+  id: string;
+  /* contact: Contact = {
+    id: '1',
+    name: 'R. Kent Jackson',
+    email: 'jacksonk@byui.edu',
+    phone: '208-496-3771',
+    description: 'This is a description.',
+    imageUrl: '../../assets/images/jacksonk.jpg',
+    group: null,
+  }; */
+
+  constructor(
+    private contactServie: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.contact = this.contactServie.getContact(this.id);
+    });
+  }
 }
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -8,27 +8,27 @@ import { ContactService } from '../contact-services/contact.service';
   styleUrl: './contact-list.component.css',
 })
 export class ContactListComponent implements OnInit {
-  // @Output() selectedContactEvent = new EventEmitter<Contact>();
-  // contacts: Contact[] = [
-  //   {
-  //     id: '1',
-  //     name: 'R. Kent Jackson',
-  //     email: 'jacksonk@byui.edu',
-  //     phone: '208-496-3771',
-  //     description: 'This is R. Kent Jackson description',
-  //     imageUrl: '../../assets/images/jacksonk.jpg',
-  //     group: null,
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Rex Barzee',
-  //     email: 'barzeer@byui.edu',
-  //     phone: '208-496-3768',
-  //     description: 'This is Rex Barzee description',
-  //     imageUrl: '../../assets/images/barzeer.jpg',
-  //     group: null,
-  //   },
-  // ];
+  /* @Output() selectedContactEvent = new EventEmitter<Contact>(); */
+  /* contacts: Contact[] = [
+    {
+      id: '1',
+      name: 'R. Kent Jackson',
+      email: 'jacksonk@byui.edu',
+      phone: '208-496-3771',
+      description: 'This is R. Kent Jackson description',
+      imageUrl: '../../assets/images/jacksonk.jpg',
+      group: null,
+    },
+    {
+      id: '2',
+      name: 'Rex Barzee',
+      email: 'barzeer@byui.edu',
+      phone: '208-496-3768',
+      description: 'This is Rex Barzee description',
+      imageUrl: '../../assets/images/barzeer.jpg',
+      group: null,
+    },
+  ]; */
   contacts: Contact[] = [];
 
   constructor(private contactService: ContactService) { }
@@ -37,9 +37,10 @@ export class ContactListComponent implements OnInit {
     this.contacts = this.contactService.getContacts();
   }
 
-  // onContactSelected(contact: Contact) {
-  //   // this.selectedContactEvent.emit(contact);
-  //   console.log(contact);
-  //   this.contactService.contactSelectedEvent.emit(contact);
-  // }
+  // OLD IMPLEMENTATION
+  /* onContactSelected(contact: Contact) {
+    // this.selectedContactEvent.emit(contact);
+    console.log(contact);
+    this.contactService.contactSelectedEvent.emit(contact);
+  } */
 }
