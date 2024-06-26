964355eae5018b63045e8c33ac09f75d42ed1310
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-06-26 17:44:19 -0300

    update # contact and document edit files base structure

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Contact } from '../../models/contact.model';
 
 @Component({
   selector: 'app-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrl: './contact-edit.component.css'
+  styleUrl: './contact-edit.component.css',
 })
 export class ContactEditComponent {
+  groupContacts: Contact[];
 
+  onCancel() {}
 }
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Document } from '../../models/document.model';
 
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
-  styleUrl: './document-edit.component.css'
+  styleUrl: './document-edit.component.css',
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit {
+  originalDocument: Document;
+  document: Document; // edited version of the document
+  editMode: boolean = false;
 
+  ngOnInit(): void {
+    return;
+  }
+  onCancel() {}
 }
